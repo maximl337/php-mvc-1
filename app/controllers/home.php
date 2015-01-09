4e2287eed4042918c82e45d027c3ce51f47901bf
ebin-antoo <ebin@ebin-antoo.com>
2015-01-09 11:42:13 -0500

    added laravel and database support to mvc

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,12 +2,32 @@
 
 class Home extends Controller
 {
+	protected $user;
+
+	public function __construct()
+    {
+   	 	$this->user = $this->model('User');	
+    }   
+
 	public function index($name = '')
 	{
 		 $user = $this->model('User');
 		 $user->name = $name;
 
 		 $this->view('home/index', ['name' => $user->name]);
+/*
+		 User::create([
+		 	'username' => 'ebin',
+		 	'email' => 'ebin@email.com'
+		 ]); */
+	}
+
+	public function create($username = '', $email = '') 
+	{
+		$this->user->create([
+			'username' => $username,
+			'email' => $email
+		]);
 	}
 
 }
